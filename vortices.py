7f53dd62daaf45377658c7d59d03ab2e22560770
Konstantinos Deligiannis <konstantinos.delis5@gmail.com>
2021-02-06 18:39:47 +0100

    Update saturday

--- vortices.py
+++ vortices.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Nov 10 15:09:50 2020
+Created on Fri Feb  5 16:26:53 2021
 
 @author: delis
 """
@@ -17,8 +17,8 @@ from qutip import *
 name_local = r'/Users/delis/Desktop/'
 name_remote = r'/scratch/konstantinos/'
 
-parallel_tasks = 16
-n_batch = 8
+parallel_tasks = 512
+n_batch = 64
 n_internal = parallel_tasks//n_batch
 qutip.settings.num_cpus = n_batch
 
@@ -41,7 +41,7 @@ ns = gammar/R
 n0 = ns*(p-1)
 nres = P/(gammar+R*n0)
 gr = 0.025
-g = 4.5
+g = 5
 
 def arrays():
     x_0 = - N * dx / 2
@@ -143,10 +143,45 @@ class model:
 # =============================================================================
 # Time evolution
 # =============================================================================
+    def vortices(self):
+        count_v = 0
+        count_av = 0
+        theta = np.angle(self.psi_x)
+        grad = np.gradient(theta, dx)
+        #v_pos = np.zeros((N, N))
+        #av_pos = np.zeros((N, N))
+        for i in range(1, N-1):
+            for j in range(1, N-1):
+                loop = (2*dx*(grad[0][j+1, i+1] - grad[1][j+1, i+1]) +
+                        2*dx*(grad[0][j+1, i-1] + grad[1][j+1, i-1]) +
+                        2*dx*(-grad[0][j-1, i-1] + grad[1][j-1, i-1]) +
+                        2*dx*(-grad[0][j-1, i+1] - grad[1][j-1, i+1]) +
+                        2*dx*(grad[0][j+1, i] + grad[1][j, i-1] - grad[0][j-1, i] - grad[1][j, i+1]))
+                if loop >= 2 * np.pi:
+                    count_v += 1
+                    #v_pos[i,j] = 1
+                elif loop <= - 2 * np.pi:
+                    count_av +=1
+                    #av_pos[i,j] = 1
+        total = count_v + count_av
+        '''
+        fig,ax = pl.subplots(1,1, figsize=(8,6))
+        xv = np.array([x[i] for i in range(N) for j in range(N) if v_pos[i,j]==1])
+        yv = np.array([x[j] for i in range(N) for j in range(N) if v_pos[i,j]==1])
+        xav = np.array([x[i] for i in range(N) for j in range(N) if av_pos[i,j]==1])
+        yav = np.array([x[j] for i in range(N) for j in range(N) if av_pos[i,j]==1])
+        ax.plot(xv, yv, 'go', markersize=2)
+        ax.plot(xav, yav, 'ro', markersize=2)
+        ax.set_xlim(x[0], x[-1])
+        ax.set_ylim(x[0], x[-1])
+        im = ax.pcolormesh(X, Y, theta, cmap='Greys')
+        pl.colorbar(im)
+        pl.show()
+        '''
+        return total
 
     def time_evolution(self, seed):
-        g1_x = np.zeros(int(N/2), dtype = complex)
-        d1_x = np.zeros(int(N/2))
+        vortexnumber = np.zeros(len(t))
         np.random.seed()
         for i in range(time_steps+1):
             #print(i)
@@ -156,24 +191,22 @@ class model:
             self.psi_mod_x = ifft2(self.psi_mod_k)
             self.psi_x *= self.prefactor_x()
             self.psi_x += np.sqrt(dt) * np.sqrt(self.sigma) * (np.random.normal(0, 1, (N,N)) + 1j*np.random.normal(0, 1, (N,N)))
-        for i in range(0, N, int(N/8)):
-            g1_x += np.conjugate(self.psi_x[i, int(N/2)]) * self.psi_x[i, int(N/2):] / 8
-            d1_x += self.n()[i, int(N/2):] / 8
-        g1_x[0] -= 1/(2*dx**2)
-        return g1_x, d1_x
+            if i>=i1 and i<=i2 and i%every==0:
+                vortexnumber[(i-i1)//every] = self.vortices()
+        return vortexnumber
 
 saveresult = r'/home6/konstantinos/'
 def g1(i_batch):
     seed = i_batch
-    correlation_batch = np.zeros((2, int(N/2)), dtype=complex)
+    vortexnumber_batch = np.zeros(len(t))
     for i_n in range(n_internal):
         gpe = model()
-        g1_x_run, d1_x_run = gpe.time_evolution(seed)
-        correlation_batch += np.vstack((g1_x_run, d1_x_run)) / n_internal
+        vortexnumber_run = gpe.time_evolution(seed)
+        vortexnumber_batch += vortexnumber_run / n_internal
         seed += n_batch
         print('The core', i_batch, 'has completed realisation number', i_n+1)
-    np.save(name_remote+'correlation_g'+str(g)+'gr'+str(gr)+os.sep+'file_core'+str(i_batch+1)+'.npy', correlation_batch)
+    np.save(name_remote+'vortices_g'+str(g)+'gr'+str(gr)+os.sep+'file_core'+str(i_batch+1)+'.npy', vortexnumber_batch)
 
 parallel_map(g1, range(n_batch))
-result = ext.ensemble_average_space(name_remote+'correlation_g'+str(g)+'gr'+str(gr), 10, int(N/2), n_batch)
-np.savetxt(saveresult+'correlation_g'+str(g)+'gr'+str(gr)+'.dat', result)
+result = ext.ensemble_average_time(name_remote+'vortices_g'+str(g)+'gr'+str(gr), t, n_batch)
+np.savetxt(saveresult+'vortices_g'+str(g)+'gr'+str(gr)+'.dat', result)
